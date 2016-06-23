f954ad45185d2051d35be6d231ed17c28b64ec52
oneRain <onerain88@gmail.com>
2016-06-23 11:55:05 +0800

    增加分辨率自适应场景，世界空间UI，自适应内容，蒙板测试场景

---
+++ Assets/Demos/02_WorldUI/WorldUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WorldUI : MonoBehaviour 
+{
+    public int cubeCount = 10;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Object cubePrefab = Resources.Load("Cube");
+        for (int i = 0; i < this.cubeCount; ++i)
+        {
+            GameObject cube = GameObject.Instantiate(cubePrefab) as GameObject;
+            cube.GetComponentInChildren<Text>().text = string.Format("Cube {0}", i);
+            Vector3 pos = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 10));
+            cube.transform.position = pos;
+        }
+	}
+	
+}
